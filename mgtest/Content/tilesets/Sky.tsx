e2967d6352cadf6d9462e0a0294684a9a770b5e5
Robby Klein <hello@robbyk.com>
2025-02-23 16:36:06 -0800

    Version unversionsed

---
+++ mgtest/Content/tilesets/Sky.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Sky" tilewidth="8" tileheight="8" tilecount="459" columns="17">
+ <image source="../sprites/sky.png" width="136" height="216"/>
+</tileset>
---
+++ mgtest/Content/tilesets/Trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Trees" tilewidth="8" tileheight="8" tilecount="2992" columns="34">
+ <image source="../sprites/trees.png" width="272" height="704"/>
+</tileset>
